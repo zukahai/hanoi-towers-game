6ca342e787d9175444db0b54d021f3076ef5db1d
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-07-13 01:23:07 +0700

    Update rectangle.js

--- js/rectangle.js
+++ js/rectangle.js
@@ -3,12 +3,23 @@ class rectangle {
         this.game = game;
         this.x = x;
         this.y = y;
-        this.size = size;
+        this.size = size - this.game.getWidth();
         this.color = color;
     }
 
     draw() {
-        // console.log(this.color);
+        this.game.context.beginPath();
+        this.game.context.fillStyle = this.color;
+        this.game.context.arc(this.x - this.size / 2, this.y, this.game.getWidth() / 2, 0, 2 * Math.PI, false);
+        this.game.context.fill();
+        this.game.context.closePath()
+
+        this.game.context.beginPath();
+        this.game.context.fillStyle = this.color;
+        this.game.context.arc(this.x + this.size / 2, this.y, this.game.getWidth() / 2, 0, 2 * Math.PI, false);
+        this.game.context.fill();
+        this.game.context.closePath()
+        
         this.game.context.fillStyle = this.color;
         this.game.context.fillRect(this.x - this.size / 2 , this.y - this.game.getWidth() / 2, this.size, this.game.getWidth() + 1);
     }
